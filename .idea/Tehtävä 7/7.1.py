c751f05d0e442a8541c6b220920cf22d3f3f8daf
hillapal <hilla.palokangas@metropolia.fi>
2025-11-04 17:44:38 +0200

    7

---
+++ .idea/Tehtävä 7/7.1.py
@@ -0,0 +1,12 @@
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = 0
+        self.kuljettu_matka = 0
+
+auto = Auto("ABC-123", 142)
+print(f"Rekisteritunnus: {auto.rekisteritunnus}")
+print(f"Huippunopeus: {auto.huippunopeus} km/h")
+print(f"Tämänhetkinen nopeus: {auto.nopeus} km/h")
+print(f"Kuljettu matka: {auto.kuljettu_matka} km")
